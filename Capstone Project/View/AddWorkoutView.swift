2302bc3cc507c28e2335810ff3af3ae8da6d9426
avqroth <142459501+avqroth@users.noreply.github.com>
2023-11-26 11:35:28 -0600

    added some custom tabs
    havent gotten them connected yet

--- Capstone Project/View/AddWorkoutView.swift
+++ Capstone Project/View/AddWorkoutView.swift
@@ -1,1 +1,20 @@
 
+import SwiftUI
+
+struct AddWorkoutView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct AddWorkoutView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
--- Capstone Project/View/HomeView.swift
+++ Capstone Project/View/HomeView.swift
@@ -2,22 +2,34 @@
 //  HomeView.swift
 //  Capstone Project
 //
-//  Created by Avery Roth on 11/12/23.
+//  Created by Avery Roth on 11/26/23.
 //
 
 import SwiftUI
 
 struct HomeView: View {
-    
-    
+    @State private var selectedTab: Tab = .house
+
+    init() {
+        UITabBar.appearance().isHidden = true
+    }
+
     var body: some View {
-        TabView {
-            Text("Home")
-                .tabItem {
-                    Image(systemName: "1.house")
-                    Text("Tab 1")
-                }
-                .tag(0)
+
+        ZStack {
+//            VStack {
+//                TabView(selection: $selectedTab) {
+//
+//                    HomeView()
+//                    SearchExerciseLibrary()
+//                    AddWorkoutView()
+//
+//                }
+//            }
+            VStack {
+                Spacer()
+                CustomTabView(selectedTab: $selectedTab)
+            }
         }
     }
 }
